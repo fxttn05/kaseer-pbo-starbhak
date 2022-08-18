6ba7a29d18fb8862f1a88bf116519f852f695e06
Fattan2005 <fattanhibrizi@gmail.com>
2022-08-18 13:30:05 +0700

    perbaikan

--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,3 @@ Route::get('/', function () {
     return view('home', ["title" => "Home"]);
 });
 
-
-Route::get('/menu', [MenuController::class, 'menu'])->name('menu');
-Route::get('/tambahmenu', [MenuController::class, 'create'])->name('tambahmenu');
-Route::post('/insertmenu', [MenuController::class, 'store'])->name('insertmenu');
-
-Route::get('/transaksi', [TransaksiController::class, 'transaksi'])->name('Transaksi');
-Route::post('/inserttransaksi', [TransaksiController::class, 'store'])->name('inserttransaksi');
-
-Route::get('/user', [UserController::class, 'user'])->name('User Page');
-
-Route::get('/login', [LoginController::class, 'login'])->name('login');
-Route::post('/loginproses', [LoginController::class, 'loginproses'])->name('loginproses');
