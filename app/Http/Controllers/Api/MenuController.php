f722db2cf95faf8e1120a7c0ab732742a9993c94
IndraDheepaBayu <indradheepa00@gmail.com>
2022-08-19 11:23:43 +0700

    API create

--- app/Http/Controllers/Api/MenuController.php
+++ app/Http/Controllers/Api/MenuController.php
@@ -6,6 +6,7 @@ use App\Models\Menu;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\MenuResource;
+use Illuminate\Support\Facades\Validator;
 
 class MenuController extends Controller
 {
@@ -16,5 +17,40 @@ class MenuController extends Controller
 
         //return collection of posts as a resource
         return new MenuResource(true, 'List Data Menu', $menus);
+    }
+
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'code_menu'   => 'required', 
+            'nama_item'   => 'required',
+            'picture'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'category'   => 'required',
+            'price'   => 'required',  
+            
+            
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //upload image
+        $image = $request->file('picture');
+        $image->storeAs('public/menus', $image->hashName());
+
+        //create post
+        $menu = Menu::create([
+            'picture'     => $image->hashName(),
+            'code_menu'     => $request->code_menu,
+            'nama_item'   => $request->nama_item,
+            'category'   => $request->category,
+            'price'   => $request->price,
+        ]);
+
+        //return response
+        return new MenuResource(true, 'Data Menu Berhasil Ditambahkan!', $menu);
     }
 }
